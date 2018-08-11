11d555a489ab8b16e2d53ac964edb97698caa254
d-german <facmet107@gmail.com>
2018-08-11 16:10:30 -0500

    Added nullable example

---
+++ NullableExamples/NullableExamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace NullableExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s1 = null;
+            var s2 = s1 ?? "nothing"; // s2 evaluates to "nothing"
+
+            StringBuilder sb = null;
+            var s = sb?.ToString().ToUpper() ?? "nothing"; // No error; s instead evaluates to "nothing"
+        }
+    }
+}
---
+++ TryParseExample/TryParseExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool continueLoop; // determines whether to keep looping
+
+        do
+        {
+            Console.Write("Enter an integer: ");
+
+            var success = int.TryParse(Console.ReadLine(), out var result);
+            var message = success ? $"You entered {result}" : "Please try again.";
+
+            Console.WriteLine(message);
+            continueLoop = !success;
+        } while (continueLoop);
+    }
+}
