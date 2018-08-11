2c5122e26827557fe16caf3ebb7be70794a670b3
d-german <facmet107@gmail.com>
2018-08-11 17:01:18 -0500

    Added exception filter example

---
+++ ExceptionFilters/ExceptionFilters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(new WebClient().DownloadString("http://google.com/"));
+        }
+        catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout)
+        {
+            Console.WriteLine("Timeout!");
+        }
+        catch (WebException ex) when (ex.Status == WebExceptionStatus.NameResolutionFailure)
+        {
+            Console.WriteLine("Name resolution failure!");
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine($"Some other failure: {ex.Status}");
+        }
+    }
+}
